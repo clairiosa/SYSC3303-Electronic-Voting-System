4ed72c2485a1c05e68a6b949ab858bb38796f769
dbews <david.bews@carleton.ca>
2015-02-26 19:30:52 -0500

    Expanded a couple function definitions as prototypes of what to be expected in the future.

--- FinalProject/communication/CommInterface.java
+++ FinalProject/communication/CommInterface.java
@@ -13,12 +13,32 @@ package FinalProject.communication;
 
 
 import FinalProject.Ballot;
+import FinalProject.persons.Candidate;
+
 import java.net.InetAddress;
 
 interface CommInterface {
 	public void connect(InetAddress parentServer, int port);
 	public void disconnect();
 
-	public void submitVote(Ballot ballot);
-	public void receiveVote(Ballot ballot);
+    /**
+     * Sends the object specified to the target.  Returns 0 if success, otherwise returns
+     * an error code.
+     *
+     * @param ballot
+     * @return
+     */
+    //TODO-dave Add way to specify target (String?)
+    public int sendMessage(Ballot ballot);
+    public int sendMessage(Candidate candidate);
+
+    /**
+     * Returns the message received.  The class using this function needs to determine
+     * the type of message being received using instanceof or getClass().
+     *
+     * @return      The object
+     */
+	public <T> T receiveMessage();
+
+    //TODO-dave Add other methods of receiving message (ie only receive ballot)
 }
