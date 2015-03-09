792ddda8c460643adc17facbb92221f2c2492492
Jonny Oommen <jonoommen@users.noreply.github.com>
2015-03-08 22:05:42 -0400

    Update Candidate.java

--- FinalProject/persons/Candidate.java
+++ FinalProject/persons/Candidate.java
@@ -11,26 +11,39 @@
 package FinalProject.persons;
 
 
-
-/**
- * Holds candidate information and related functions.
- *
- * Extends Voter as candidates are allowed to cast a ballot.
- *
- */
-public class Candidate extends Voter{
-
-	String party;
-
-
-	/**
-	 * Empty constructor.
-	 */
-	public Candidate(){
-		super();
-		party = "";
+   class Candidate extends Voter {
+	private int voteCount;
+	private double votingPercentage;
+	private static int totalVotes;
+	
+	Candidate(String name, String party) {
+		super(name, party);
+		voteCount = 0;
+		votingPercentage=0.0;
 	}
+	
 
-	public void setParty() {}
-	public String getParty() { return party; }
-}
+	public int getVoteCount(){
+		return voteCount;
+	}
+	
+	public void addVote(){
+		voteCount++; 
+		totalVotes++;
+	}
+	public double getVotingPercentage(){
+		return votingPercentage;
+	}
+	public boolean setVotingPercentage(double amt){
+		if (amt >=0 && amt<=100){
+			votingPercentage=amt; 
+			return true; 
+		}
+		return false;
+	}
+	
+	public int getTotalVotes(){
+		return totalVotes;
+	}
+	
+   }
