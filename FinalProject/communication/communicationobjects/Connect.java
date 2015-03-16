dfda489b9d0e47dd6e2450a8ca3f1db7c415168f
dbews <david.bews@carleton.ca>
2015-03-16 12:21:08 -0400

    Added some comments, still basically empty tho.

--- FinalProject/communication/communicationobjects/Connect.java
+++ FinalProject/communication/communicationobjects/Connect.java
@@ -4,6 +4,8 @@
  *
  *	Ack.java
  *
+ * Intended to be sent by Comm so implements Serializable.
+ * Serializable makes it so the object can be turned into an array of bytes by Packets.java's functions.
  */
 
 
