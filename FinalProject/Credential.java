9f1b0d537c1505bf74f4bafd33cbff02ce8841b5
unknown <damian.polan@gmail.com>
2015-03-18 12:08:42 -0400

    District Server now supports credentials

--- FinalProject/Credential.java
+++ FinalProject/Credential.java
@@ -3,7 +3,32 @@ package FinalProject;
 import java.io.Serializable;
 
 public class Credential implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	private String user;
 	private String pin;
-	
+
+	public Credential(String user, String pin) {
+		super();
+		this.user = user;
+		this.pin = pin;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+
 }
